8eb926dbc801cfcaedb1744433b30cf31399f631
grillopress <grillopress@gmail.com>
2018-08-21 10:24:42 +0100

    update spinner to centre

--- public/javascripts/nhsonline/spinner.js
+++ public/javascripts/nhsonline/spinner.js
@@ -2,6 +2,6 @@ var THE_PROTOTYPE_URL = "/nhsonline";
 
 $(document).ready(function(){
   window.setTimeout(function() {
-      window.location.href = THE_PROTOTYPE_URL + "/service/your-current-choice";
+    window.location.href = THE_PROTOTYPE_URL + "/service/your-current-choice";
   }, 3000);
 });
