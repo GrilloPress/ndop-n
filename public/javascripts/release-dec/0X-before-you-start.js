147a65e1fc81691c602253b9bb923dd441bbdfef
grillopress <grillopress@gmail.com>
2019-01-23 08:04:32 +0000

    align content with live

---
+++ public/javascripts/release-dec/0X-before-you-start.js
@@ -0,0 +1,9 @@
+var THE_THE_PROTOTYPE_URL = "/release-dec";
+
+// resets preferences on the service start page on register your preference page
+//
+if ( sessionStorage.singlePref ) {
+
+  sessionStorage.singlePref = "";
+
+}
